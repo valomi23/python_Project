e0acff5cc0e0a4a3ed8249cd3eade43110c1a267
valomi23 <vanya.lototsky2016@gmail.com>
2022-08-15 00:02:01 +0300

    1

--- сума_списку.py
+++
@@ -1,4 +0,0 @@
-s = [2, -1, 9, 6]
-print(s)
-suma_s = sum(s)
-print(suma_s)
--- тривалість поїздки.py
+++
@@ -1,8 +0,0 @@
-kanikul = int(input("Число днів:"))
-god = kanikul*24
-hv = god*60
-sec = hv*60
-
-print("{:<10} {:<10} {:<10}".format(god, hv, sec))
-
-#print(f"Пройшло {god} годин, {hv} хвилин, {sec} секунд канікул!")
